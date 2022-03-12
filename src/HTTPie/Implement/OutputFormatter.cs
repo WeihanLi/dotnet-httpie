e404c5acb7e58d6c858e1241b2ac2f573effee9a
Weihan Li <weihanli@outlook.com>
2022-03-12 16:40:48 +0800

    Use Math.Net to calculate statics

--- src/HTTPie/Implement/OutputFormatter.cs
+++ src/HTTPie/Implement/OutputFormatter.cs
@@ -4,6 +4,7 @@
 using HTTPie.Abstractions;
 using HTTPie.Models;
 using HTTPie.Utilities;
+using MathNet.Numerics.Statistics;
 using Microsoft.Extensions.Primitives;
 using System.Text;
 using System.Text.Json.Nodes;
@@ -151,10 +152,14 @@ public class OutputFormatter : IOutputFormatter
             SuccessRequestCount = responseList.Count(x => x.Response.IsSuccessStatusCode),
             Average = durationInMs.Average(),
             TotalElapsed = totalElapsed,
-            P99 = Percentile(durationInMs, 0.99),
-            P95 = Percentile(durationInMs, 0.95),
-            P90 = Percentile(durationInMs, 0.9),
-            P50 = Percentile(durationInMs, 0.5),
+            Min = SortedArrayStatistics.Minimum(durationInMs),
+            Max = SortedArrayStatistics.Maximum(durationInMs),
+            Median = SortedArrayStatistics.Median(durationInMs),
+            P99 = SortedArrayStatistics.Quantile(durationInMs, 0.99),
+            P95 = SortedArrayStatistics.Quantile(durationInMs, 0.95),
+            P90 = SortedArrayStatistics.Quantile(durationInMs, 0.90),
+            P75 = SortedArrayStatistics.Quantile(durationInMs, 0.75),
+            P50 = SortedArrayStatistics.Quantile(durationInMs, 0.50),
         };
 
         return $@"{GetCommonOutput(httpContext, outputFormat & OutputFormat.RequestInfo)}
@@ -162,27 +167,16 @@ Total request: {reportModel.TotalRequestCount}({reportModel.TotalElapsed} ms), s
 
 Request duration:
 Requests per second: {reportModel.RequestsPerSecond}
-Average: {reportModel.Average} ms
-P99: {reportModel.P99} ms
-P95: {reportModel.P95} ms
-P90: {reportModel.P90} ms
-P50: {reportModel.P50} ms
+{nameof(reportModel.Min)}: {reportModel.Min} ms
+{nameof(reportModel.Max)}: {reportModel.Max} ms
+{nameof(reportModel.Median)}: {reportModel.Median} ms
+{nameof(reportModel.Average)}: {reportModel.Average} ms
+{nameof(reportModel.P99)}: {reportModel.P99} ms
+{nameof(reportModel.P95)}: {reportModel.P95} ms
+{nameof(reportModel.P90)}: {reportModel.P90} ms
+{nameof(reportModel.P75)}: {reportModel.P75} ms
+{nameof(reportModel.P50)}: {reportModel.P50} ms
 ";
-    }
-
-    // https://stackoverflow.com/questions/8137391/percentile-calculation
-    // https://en.wikipedia.org/wiki/Percentile#Calculation_methods
-    private static double Percentile(double[] sequence, double percentile)
-    {
-        // Array.Sort(sequence);
-        var len = sequence.Length;
-        var n = (len - 1) * percentile + 1;
-        var idx = (int)n;
-        // Another method: double n = (N + 1) * excelPercentile;
-        if (idx == 1) return sequence[0];
-        if (idx == len) return sequence[len - 1];
-        var d = n - idx;
-        return sequence[idx - 1] + d * (sequence[idx] - sequence[idx - 1]);
     }
 
     private static string Prettify(string body, PrettyOptions prettyOption)
--- src/HTTPie/Models/LoadTestReportModel.cs
+++ src/HTTPie/Models/LoadTestReportModel.cs
@@ -12,8 +12,12 @@ public record LoadTestReportModel
     public double TotalElapsed { get; init; }
     public double RequestsPerSecond => TotalRequestCount * 1000 / TotalElapsed;
     public double Average { get; init; }
+    public double Min { get; init; }
+    public double Max { get; init; }
+    public double Median { get; init; }
     public double P99 { get; init; }
     public double P95 { get; init; }
     public double P90 { get; init; }
+    public double P75 { get; init; }
     public double P50 { get; init; }
 }
