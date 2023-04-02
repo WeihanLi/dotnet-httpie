352b4e67003a09e1b132e85e4ad01627a950bd9e
Weihan Li <weihanli@outlook.com>
2023-04-02 10:55:06 +0800

    refactor: add trim for request header value

--- src/HTTPie/Middleware/RequestHeadersMiddleware.cs
+++ src/HTTPie/Middleware/RequestHeadersMiddleware.cs
@@ -21,7 +21,7 @@ public sealed class RequestHeadersMiddleware : IRequestMiddleware
                           && item[..index].IsMatch(Constants.ParamNameRegex))
             {
                 var key = item[..index];
-                var value = item[(index + 1)..];
+                var value = item[(index + 1)..].Trim();
                 if (requestModel.Headers.TryGetValue(key, out var values))
                     requestModel.Headers[key] =
                         new StringValues(values.ToArray().Append(value).ToArray());
